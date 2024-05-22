c7da282885bf18da6b0c9a460d57ef0185d3f87f
ejsong25 <ejsong25@gmail.com>
2024-05-22 14:58:33 +0900

    drop dirtydata

--- code/csi_csv_visualization.py
+++ code/csi_csv_visualization.py
@@ -4,16 +4,46 @@ from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
 
-df = pd.read_csv(f"csvData/output_stand_1.csv")
-for i in range(0, 64):
-    scaler = StandardScaler()
-    df_scaled = scaler.fit_transform(
-        df[f'Sub {i} RXTX 0/0'].values.reshape(1, -1))
-    plt.plot(df.index, df_scaled)
+df = pd.read_csv(f"csvData/output_stand.csv")
+# -inf와 inf 값을 NaN으로 대체
+df.replace([float('-inf'), float('inf')], float('nan'), inplace=True)
+
+# NaN이 포함된 행을 삭제
+df = df.dropna(axis=0, how='any')
+print(df)
+
+# for i in range(0, 64):
+# 시간 시리즈 생성 (데이터의 행 수만큼)
+time_series = range(len(df))
+
+# 그래프 크기 설정
+# plt.figure(figsize=(14, 8))
+
+# 각 서브캐리어 값을 시간에 따라 플롯
+for column in df.columns:
+    plt.plot(time_series, df[column], label=column)
+    # 그래프 라벨 설정
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
-    plt.title(f'{a}')
+    plt.title('Stand')
+
+    # 범례 위치 설정
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    # 그리드 및 레이아웃 설정
+    plt.grid(True)
+    plt.tight_layout()
+
+    # 그래프 표시
     plt.show()
+    # scaler = StandardScaler()
+    # df_scaled = scaler.fit_transform(
+    #     df[f'Sub {i} RXTX 0/0'].values.reshape(1, -1))
+    # plt.plot(df.index, df_scaled)
+    # plt.xlabel('Time')
+    # plt.ylabel('Amplitude')
+    # plt.title(f'stand')
+    # plt.show()
 
 """
 b = ['sit','empty','fall','walk','stand']
